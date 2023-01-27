47e5ba84c790e35866ece009a22e8e0ecbb4aabd
Nullpointer <mikeschulze.lpz@kabelmail.de>
2023-01-27 23:08:44 +0100

    GD-88: Fix script runtime error detection (#92)
    # Why
    When running test without debug info script errors was ignored and
    results into success
    
    # What
    - Fix the error monitor to parse godot.log for script errors
    - Fix function await assert runtime errors
    - Fix runtime error when test for `is_engine_type`
    - rework on test timeout report generation
    - Moved reg_ex helper func to 'GdUnitTools.to_regex'
    - Removed invalid prefix 'timeout!' from tree inspector when a test has
    state error

--- addons/gdUnit4/test/monitor/GodotGdErrorMonitorTest.gd
+++ addons/gdUnit4/test/monitor/GodotGdErrorMonitorTest.gd
@@ -9,56 +9,53 @@ const __source = 'res://addons/gdUnit4/src/monitor/GodotGdErrorMonitor.gd'
 
 
 const error_report = """
-	**ERROR**: Error parsing JSON at line 0: 
-   At: core/bind/core_bind.cpp:3293:parse() - Error parsing JSON at line 0: 
+	USER ERROR: this is an error
+	   at: push_error (core/variant/variant_utility.cpp:880)
 	"""
 const script_error = """
-	**SCRIPT ERROR**: Invalid call. Nonexistent function 'foo' in base 'RefCounted'.
-   At: res://test_test/test_test.gd:14:TestTest.test_with_script_errors() - Invalid call. Nonexistent function 'foo' in base 'RefCounted'.
-	"""
+	USER SCRIPT ERROR: Trying to call a function on a previously freed instance.
+	   at: GdUnitScriptTypeTest.test_xx (res://addons/gdUnit4/test/GdUnitScriptTypeTest.gd:22)
+"""
+
 
 func test_parse_script_error_line_number() -> void:
-	var line := GodotGdErrorMonitor.new()._parse_error_line_number(script_error)
-	assert_int(line).is_equal(14)
+	var line := GodotGdErrorMonitor.new()._parse_error_line_number(script_error.dedent())
+	assert_int(line).is_equal(22)
+
 
 func test_parse_push_error_line_number() -> void:
-	var line := GodotGdErrorMonitor.new()._parse_error_line_number(error_report)
+	var line := GodotGdErrorMonitor.new()._parse_error_line_number(error_report.dedent())
 	assert_int(line).is_equal(-1)
+
 
 func test_scan_for_push_errors() -> void:
 	var monitor := mock(GodotGdErrorMonitor, CALL_REAL_FUNC) as GodotGdErrorMonitor
 	monitor._report_enabled = true
-	do_return(error_report).checked(monitor)._collect_seek_log()
+	do_return(error_report.dedent().split('\n')).checked(monitor)._collect_log_entries()
 	
 	# with disabled push_error reporting
 	do_return(false).checked(monitor)._is_report_push_errors()
-	monitor._scan_for_errors()
 	assert_array(monitor.reports()).is_empty()
 	
 	# with enabled push_error reporting
 	do_return(true).checked(monitor)._is_report_push_errors()
-	monitor._scan_for_errors()
-	var error =\
-"""	**ERROR**: Error parsing JSON at line 0: 
-   At: core/bind/core_bind.cpp:3293:parse() - Error parsing JSON at line 0: """
-	var expected_report = GdUnitReport.new().create(GdUnitReport.FAILURE, -1, error)
+	
+	var expected_report := GodotGdErrorMonitor._report_user_error("USER ERROR: this is an error", "at: push_error (core/variant/variant_utility.cpp:880)")
 	assert_array(monitor.reports()).contains_exactly([expected_report])
+
 
 func test_scan_for_script_errors() -> void:
 	var monitor := mock(GodotGdErrorMonitor, CALL_REAL_FUNC) as GodotGdErrorMonitor
 	monitor._report_enabled = true
-	do_return(script_error).checked(monitor)._collect_seek_log()
+	do_return(script_error.dedent().split('\n')).checked(monitor)._collect_log_entries()
 	
 	# with disabled push_error reporting
 	do_return(false).checked(monitor)._is_report_script_errors()
-	monitor._scan_for_errors()
 	assert_array(monitor.reports()).is_empty()
 	
 	# with enabled push_error reporting
 	do_return(true).checked(monitor)._is_report_script_errors()
-	monitor._scan_for_errors()
-	var error =\
-"""	**SCRIPT ERROR**: Invalid call. Nonexistent function 'foo' in base 'RefCounted'.
-   At: res://test_test/test_test.gd:14:TestTest.test_with_script_errors() - Invalid call. Nonexistent function 'foo' in base 'RefCounted'."""
-	var expected_report = GdUnitReport.new().create(GdUnitReport.FAILURE, 14, error)
+	
+	var expected_report := GodotGdErrorMonitor._report_runtime_error("USER SCRIPT ERROR: Trying to call a function on a previously freed instance.",\
+		"at: GdUnitScriptTypeTest.test_xx (res://addons/gdUnit4/test/GdUnitScriptTypeTest.gd:22)")
 	assert_array(monitor.reports()).contains_exactly([expected_report])
