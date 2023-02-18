f44d5f90255ac5ca022f965e910bbdb7bd418a8d
Mike Schulze <mikeschulze.lpz@kabelmail.de>
2023-02-18 11:01:08 +0100

    GD-120: Fix signal 11 crash when execute tests via `GdUnitCmdTool`
    # Why
    Running the `GdUnitCmdTool` ends with an signal 11 and many orphan resource detected
    
    
    # What
    - fixed the signal 11 by (i would say is a Godot core bug) by:
       - moving the func `assert_failed_at` to another posision (very strange findings)
       - using typed array on `load_testsuites()`
       - fix some orphan issues around singleton implementation
       - centralize code to finaly cleanup in new `quit` function to avoid orphan nodes
       - format touched code according to code style

--- addons/gdUnit4/src/core/GdUnitSingleton.gd
+++ addons/gdUnit4/src/core/GdUnitSingleton.gd
@@ -8,24 +8,36 @@ class_name GdUnitSingleton
 extends RefCounted
 
 
+const MEATA_KEY := "GdUnitSingletons"
+
+
 static func instance(name :String, clazz :Callable) -> Variant:
 	if Engine.has_meta(name):
 		return Engine.get_meta(name)
 	var singleton := clazz.call()
 	Engine.set_meta(name, singleton)
 	GdUnitTools.prints_verbose("Register singleton '%s:%s'" % [name, singleton])
-	var singletons := Engine.get_meta("GdUnitSingeltons", PackedStringArray())
+	var singletons := Engine.get_meta(MEATA_KEY, PackedStringArray())
 	singletons.append(name)
-	Engine.set_meta("GdUnitSingeltons", singletons)
+	Engine.set_meta(MEATA_KEY, singletons)
 	return singleton
 
 
-static func dispose() -> void:
-	GdUnitTools.prints_verbose("Cleanup singleton references")
-	var singletons := Engine.get_meta("GdUnitSingeltons", PackedStringArray())
-	for singleton in singletons:
+static func unregister(singleton :String) -> void:
+	var singletons :PackedStringArray = Engine.get_meta(MEATA_KEY, PackedStringArray())
+	if singletons.has(singleton):
+		var index := singletons.find(singleton)
+		singletons.remove_at(index)
 		var instance := Engine.get_meta(singleton)
 		GdUnitTools.prints_verbose("Free singeleton '%s:%s'" % [singleton, instance])
 		GdUnitTools.free_instance(instance)
 		Engine.remove_meta(singleton)
-	Engine.remove_meta("GdUnitSingeltons")
+	Engine.set_meta(MEATA_KEY, singletons)
+
+
+static func dispose() -> void:
+	GdUnitTools.prints_verbose("Cleanup singletons ..")
+	var singletons := Engine.get_meta(MEATA_KEY, PackedStringArray())
+	for singleton in singletons:
+		unregister(singleton)
+	Engine.remove_meta(MEATA_KEY)
