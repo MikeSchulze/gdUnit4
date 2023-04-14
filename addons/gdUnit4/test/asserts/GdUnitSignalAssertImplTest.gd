9b8e078f4a7bf3c4547aa044b9a6da967c7feaa8
Mike Schulze <mikeschulze.lpz@kabelmail.de>
2023-04-14 21:34:45 +0200

    append

--- addons/gdUnit4/test/asserts/GdUnitSignalAssertImplTest.gd
+++ addons/gdUnit4/test/asserts/GdUnitSignalAssertImplTest.gd
@@ -42,6 +42,11 @@ func after():
 		.is_true()
 
 
+# we need to skip await fail test because of an bug in Godot 4.0 stable
+func is_skip_fail_await() -> bool:
+	return Engine.get_version_info().hex < 0x40002
+
+
 # using this helper to await for the given callable and assert the failure
 func verify_failed(cb :Callable) -> GdUnitStringAssert:
 	GdAssertReports.expect_fail(true)
@@ -68,6 +73,10 @@ func test_signal_is_emitted_without_args() -> void:
 	# wait until signal 'test_signal_counted' without args
 	await assert_signal(signal_emitter).is_emitted("test_signal")
 	# wait until signal 'test_signal_unused' where is never emitted
+	
+	if is_skip_fail_await():
+		return
+
 	(await verify_failed(func(): await assert_signal(signal_emitter).wait_until(500).is_emitted("test_signal_unused")))\
 		.is_equal("Expecting emit signal: 'test_signal_unused()' but timed out after 500ms")
 
@@ -76,6 +85,8 @@ func test_signal_is_emitted_with_args() -> void:
 	# wait until signal 'test_signal' is emitted with value 30
 	await assert_signal(signal_emitter).is_emitted("test_signal_counted", [20])
 	
+	if is_skip_fail_await():
+		return
 	(await verify_failed(func(): await assert_signal(signal_emitter).wait_until(50).is_emitted("test_signal_counted", [500]))) \
 		.is_equal("Expecting emit signal: 'test_signal_counted([500])' but timed out after 50ms")
 
@@ -85,12 +96,18 @@ func test_signal_is_not_emitted() -> void:
 	await assert_signal(signal_emitter).wait_until(50).is_not_emitted("test_signal_counted")
 	# wait to verify signal 'test_signal_counted(50)' is not emitted until the NEXT first 100ms
 	await assert_signal(signal_emitter).wait_until(50).is_not_emitted("test_signal_counted", [50])
+	
+	if is_skip_fail_await():
+		return
 	# until the next 500ms the signal is emitted and ends in a failure
 	(await verify_failed(func(): await assert_signal(signal_emitter).wait_until(1000).is_not_emitted("test_signal_counted", [50]))) \
 		.starts_with("Expecting do not emit signal: 'test_signal_counted([50])' but is emitted after")
 
 
 func test_override_failure_message() -> void:
+	if is_skip_fail_await():
+		return
+	
 	(await verify_failed(func(): await assert_signal(signal_emitter) \
 		.override_failure_message("Custom failure message")\
 		.wait_until(100)\
@@ -109,8 +126,9 @@ func test_node_changed_emitting_signals():
 	
 	# expecting to fail, we not changed the visibility
 	#node.visible = true;
-	(await verify_failed(func(): await assert_signal(node).wait_until(200).is_emitted("visibility_changed")))\
-		.is_equal("Expecting emit signal: 'visibility_changed()' but timed out after 200ms")
+	if not is_skip_fail_await():
+		(await verify_failed(func(): await assert_signal(node).wait_until(200).is_emitted("visibility_changed")))\
+			.is_equal("Expecting emit signal: 'visibility_changed()' but timed out after 200ms")
 	
 	node.show()
 	await assert_signal(node).wait_until(200).is_emitted("draw")
@@ -126,5 +144,8 @@ func test_is_signal_exists() -> void:
 		.is_signal_exists("tree_exiting")\
 		.is_signal_exists("tree_exited")
 	
+	if is_skip_fail_await():
+		return
+
 	(await verify_failed(func(): assert_signal(node).is_signal_exists("not_existing_signal"))) \
 		.is_equal("The signal 'not_existing_signal' not exists checked object 'Node2D'.")
