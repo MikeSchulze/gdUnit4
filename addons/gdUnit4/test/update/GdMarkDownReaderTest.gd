3439939e03eeae60b69a417ed6a264c00326340d
Nullpointer <mikeschulze.lpz@kabelmail.de>
2023-06-19 21:15:54 +0200

    GD-194: Fix load/restart errors on GdUnit update (#219)
    # Why
    The update process was buggy and the Godot editor was not restarted
    automatically after the update.
    
    # What
    - rework `GdMarkDownReader`
      - fix image handling, save always as PNG
      - fix render header 1 and 2 to show a line below
      - code cleanup 
    - rework on GdUnit upgrade tool
    - improve the upgrade dialog
    - move download part to upgrade tool
    
    
    ![image](https://github.com/MikeSchulze/gdUnit4/assets/347037/84d4c905-fe8d-4181-9cc3-6fae2b8c18cf)
    
    
    ![image](https://github.com/MikeSchulze/gdUnit4/assets/347037/efea4ee3-a3f4-46df-960d-9ca17a7bce17)
    
    
    ![image](https://github.com/MikeSchulze/gdUnit4/assets/347037/002db6cd-a142-4e97-8413-f8a5e0a45fe2)

--- addons/gdUnit4/test/update/GdMarkDownReaderTest.gd
+++ addons/gdUnit4/test/update/GdMarkDownReaderTest.gd
@@ -32,26 +32,32 @@ func test_tobbcode_table() -> void:
 	assert_str(await _reader.to_bbcode(source)).is_equal(expected)
 
 
-func test_tobbcode_headers() -> void:
+func test_tobbcode_html_headers() -> void:
 	var source := resource_as_string("res://addons/gdUnit4/test/update/resources/html_header.txt")
 	var expected := resource_as_string("res://addons/gdUnit4/test/update/resources/bbcode_header.txt")
 	assert_str(await _reader.to_bbcode(source)).is_equal(expected)
 
 
+func test_tobbcode_md_headers() -> void:
+	var source := resource_as_string("res://addons/gdUnit4/test/update/resources/md_header.txt")
+	var expected := resource_as_string("res://addons/gdUnit4/test/update/resources/bbcode_md_header.txt")
+	assert_str(await _reader.to_bbcode(source)).is_equal(expected)
+
+
 func test_tobbcode_list() -> void:
-	assert_str(await _reader.to_bbcode("- item")).is_equal("[img=12x12]res://addons/gdUnit4/src/update/assets/dot1.png[/img] item")
-	assert_str(await _reader.to_bbcode("  - item")).is_equal("   [img=12x12]res://addons/gdUnit4/src/update/assets/dot2.png[/img] item")
-	assert_str(await _reader.to_bbcode("    - item")).is_equal("      [img=12x12]res://addons/gdUnit4/src/update/assets/dot1.png[/img] item")
-	assert_str(await _reader.to_bbcode("      - item")).is_equal("         [img=12x12]res://addons/gdUnit4/src/update/assets/dot2.png[/img] item")
+	assert_str(await _reader.to_bbcode("- item")).is_equal("[img=12x12]res://addons/gdUnit4/src/update/assets/dot1.png[/img] item\n")
+	assert_str(await _reader.to_bbcode("  - item")).is_equal("   [img=12x12]res://addons/gdUnit4/src/update/assets/dot2.png[/img] item\n")
+	assert_str(await _reader.to_bbcode("    - item")).is_equal("      [img=12x12]res://addons/gdUnit4/src/update/assets/dot1.png[/img] item\n")
+	assert_str(await _reader.to_bbcode("      - item")).is_equal("         [img=12x12]res://addons/gdUnit4/src/update/assets/dot2.png[/img] item\n")
 
 
 func test_to_bbcode_embeded_text() -> void:
-	assert_str(await _reader.to_bbcode("> some text")).is_equal("[img=50x14]res://addons/gdUnit4/src/update/assets/embedded.png[/img][i] some text[/i]")
+	assert_str(await _reader.to_bbcode("> some text")).is_equal("[img=50x14]res://addons/gdUnit4/src/update/assets/embedded.png[/img][i] some text[/i]\n")
 
 
 func test_process_image() -> void:
 	#regex("!\\[(.*?)\\]\\((.*?)(( )+(.*?))?\\)")
-	var reg_ex :RegEx = _reader.md_replace_patterns[19][0]
+	var reg_ex :RegEx = _reader.md_replace_patterns[24][0]
 	
 	# without tooltip
 	assert_str(await _reader.process_image(reg_ex, "![alt text](res://addons/gdUnit4/test/update/resources/icon48.png)"))\
@@ -68,9 +74,9 @@ func test_process_image() -> void:
 		""".dedent()
 	var expected := """
 		[img]res://addons/gdUnit4/test/update/resources/icon48.png[/img]
-
+		
 		[img]res://addons/gdUnit4/test/update/resources/icon23.png[/img]
-
+		
 		""".dedent()
 	assert_str(await _reader.process_image(reg_ex, input))\
 		.is_equal(expected)
@@ -78,24 +84,39 @@ func test_process_image() -> void:
 
 func test_process_image_by_reference() -> void:
 	#regex("!\\[(.*?)\\]\\((.*?)(( )+(.*?))?\\)")
-	var reg_ex :RegEx = _reader.md_replace_patterns[18][0]
+	var reg_ex :RegEx = _reader.md_replace_patterns[23][0]
 	var input := """
 		![alt text1][logo-1]
-
+		
 		[logo-1]:https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 2"
-
+		
 		![alt text2][logo-1]
-
+		
 		""".dedent()
-
+	
 	var expected := """
 		![alt text1](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png)
-
-
+		
+		
 		![alt text2](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png)
-
+		
 		""".replace("\r", "").dedent()
 	
 	# without tooltip
 	assert_str(_reader.process_image_references(reg_ex, input))\
 		.is_equal(expected)
+
+
+func test_process_external_image_save_as_png() -> void:
+	var input := """
+		[img]https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png[/img]
+		[img]https://github.com/MikeSchulze/gdUnit4/assets/347037/3205c9f1-1746-4716-aa6d-e3a1808b761d[/img]
+		""".dedent()
+	
+	var output := await _reader._process_external_image_resources(input)
+	assert_str(output).is_equal("""
+		[img]res://addons/gdUnit4/tmp-update/icon48.png[/img]
+		[img]res://addons/gdUnit4/tmp-update/3205c9f1-1746-4716-aa6d-e3a1808b761d.png[/img]
+		""".dedent())
+	assert_file("res://addons/gdUnit4/tmp-update/icon48.png").exists()
+	assert_file("res://addons/gdUnit4/tmp-update/3205c9f1-1746-4716-aa6d-e3a1808b761d.png").exists()
