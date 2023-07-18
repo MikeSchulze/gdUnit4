MikeSchulze/gdUnit4

commit e273029d5a076bd1dbbb6bb467af344d7dbdc1e3

GD-122: Fix orphan nodes (#137)
# Why
When running the GdUnit Cmd line tool, there are many orphaned nodes.

# What
- fixed problems with timer registration
- fixed auto_free not to register GdNativeClass
- added a workaround for
https://github.com/godotengine/godot/issues/73889
- register an assert to hold the instance and release it manually after
test execution
  

# known issues
there are still orphan nodes left
[issue 144](https://github.com/MikeSchulze/gdUnit4/issues/144)

addons/gdUnit4/src/asserts/GdUnitFuncAssertImpl.gd
@@ -15,8 +15,12 @@ var _timeout := DEFAULT_TIMEOUT
 var _expect_result :int
 var _interrupted := false
 
+var _sleep_timer :Timer = null
+
 
 func _init(instance :Object, func_name :String, args := Array(), expect_result := EXPECT_SUCCESS):
+	# temporary workaround to hold the instance see https://github.com/godotengine/godot/issues/73889
+	GdUnitTools.register_assert(self)
 	_line_number = GdUnitAssertImpl._get_line_number()
 	_expect_result = expect_result
 	GdAssertReports.reset_last_error_line_number()
@@ -28,6 +32,18 @@ func _init(instance :Object, func_name :String, args := Array(), expect_result :
 		report_error("The function '%s' do not exists checked instance '%s'." % [func_name, instance])
 	else:
 		_current_value_provider = CallBackValueProvider.new(instance, func_name, args)
+
+
+func _to_string():
+	return "GdUnitFuncAssertImpl" + str(get_instance_id())
+
+
+func _notification(_what):
+	if is_instance_valid(self):
+		dispose()
+	# temporary workaround to hold the instance see https://github.com/godotengine/godot/issues/73889
+	while is_instance_valid(self) and get_reference_count() > 1:
+		unreference()
 
 
 func report_success() -> GdUnitAssert:
@@ -112,7 +128,7 @@ func _validate_callback(predicate :Callable, expected = null) -> GdUnitFuncAsser
 		return self
 	var time_scale = Engine.get_time_scale()
 	var timer := Timer.new()
-	timer.set_name("gdunit_interrupt_timer_%d" % timer.get_instance_id())
+	timer.set_name("gdunit_funcassert_interrupt_timer_%d" % timer.get_instance_id())
 	Engine.get_main_loop().root.add_child(timer)
 	timer.add_to_group("GdUnitTimers")
 	timer.timeout.connect(func do_interrupt():
@@ -121,9 +137,9 @@ func _validate_callback(predicate :Callable, expected = null) -> GdUnitFuncAsser
 		, CONNECT_REFERENCE_COUNTED)
 	timer.set_one_shot(true)
 	timer.start((_timeout/1000.0)*time_scale)
-	var sleep := Timer.new()
-	sleep.set_name("gdunit_sleep_timer_%d" % sleep.get_instance_id() )
-	Engine.get_main_loop().root.add_child(sleep)
+	_sleep_timer = Timer.new()
+	_sleep_timer.set_name("gdunit_funcassert_sleep_timer_%d" % _sleep_timer.get_instance_id() )
+	Engine.get_main_loop().root.add_child(_sleep_timer)
 	
 	while true:
 		next_current_value()
@@ -133,12 +149,11 @@ func _validate_callback(predicate :Callable, expected = null) -> GdUnitFuncAsser
 		var is_success = predicate.call(current, expected)
 		if _expect_result != EXPECT_FAIL and is_success:
 			break
-		sleep.start(0.05)
-		await sleep.timeout
+		if is_instance_valid(_sleep_timer):
+			_sleep_timer.start(0.05)
+			await _sleep_timer.timeout
 	
-	sleep.stop()
-	sleep.queue_free()
-	
+	_sleep_timer.stop()
 	await Engine.get_main_loop().process_frame
 	dispose()
 	if _interrupted:
@@ -151,14 +166,20 @@ func _validate_callback(predicate :Callable, expected = null) -> GdUnitFuncAsser
 	return self
 
 
-@warning_ignore("redundant_await")
 func next_current_value():
-	var current = await _current_value_provider.get_value()
-	call_deferred("emit_signal", "value_provided", current)
+	@warning_ignore("redundant_await")
+	if is_instance_valid(_current_value_provider):
+		var current = await _current_value_provider.get_value()
+		call_deferred("emit_signal", "value_provided", current)
 
 
 # it is important to free all references/connections to prevent orphan nodes
 func dispose():
-	GdUnitTools.release_connections(self)
-	_current_value_provider.dispose()
-	_current_value_provider = null
+	GdUnitTools._release_connections(self)
+	if is_instance_valid(_current_value_provider):
+		_current_value_provider.dispose()
+		_current_value_provider = null
+	if is_instance_valid(_sleep_timer):
+		_sleep_timer.stop()
+		_sleep_timer.free()
+		_sleep_timer = null

addons/gdUnit4/src/core/GdUnitTools.gd
@@ -194,16 +194,26 @@ static func prints_verbose(message :String) -> void:
 
 
 static func free_instance(instance :Variant) -> bool:
-	# is instance already freed?
-	if not is_instance_valid(instance) or ClassDB.class_get_property(instance, "new"):
+	if instance is Array:
+		for element in instance:
+			free_instance(element)
+		instance.clear()
+		return true
+	# do not free an already freed instance
+	if not is_instance_valid(instance):
 		return false
-	
-	release_double(instance)
-	if instance is RefCounted:
+	# do not free a class refernece
+	if typeof(instance) == TYPE_OBJECT and (instance as Object).is_class("GDScriptNativeClass"):
+		return false
+	if is_instance_valid(instance) and instance is RefCounted:
 		instance.notification(Object.NOTIFICATION_PREDELETE)
 		return true
 	else:
-		release_connections(instance)
+			# is instance already freed?
+		if not is_instance_valid(instance) or ClassDB.class_get_property(instance, "new"):
+			return false
+		release_double(instance)
+		#release_connections(instance)
 		if instance is Timer:
 			instance.stop()
 			#instance.queue_free()
@@ -213,7 +223,7 @@ static func free_instance(instance :Variant) -> bool:
 		return !is_instance_valid(instance)
 
 
-static func release_connections(instance :Object):
+static func _release_connections(instance :Object):
 	if is_instance_valid(instance):
 		# disconnect from all connected signals to force freeing, otherwise it ends up in orphans
 		for connection in instance.get_incoming_connections():
@@ -223,14 +233,41 @@ static func release_connections(instance :Object):
 			#prints("signal", signal_.get_name(), signal_.get_object())
 			#prints("callable", callable_.get_object())
 			if instance.has_signal(signal_.get_name()) and instance.is_connected(signal_.get_name(), callable_):
+				#prints("disconnect signal", signal_.get_name(), callable_)
 				instance.disconnect(signal_.get_name(), callable_)
-	
+	release_timers()
+
+
+static func release_timers():
 	# we go the new way to hold all gdunit timers in group 'GdUnitTimers'
 	for node in Engine.get_main_loop().root.get_children():
 		if node.is_in_group("GdUnitTimers"):
+			#prints("found gdunit timer artifact", node, is_instance_valid(node))
 			if is_instance_valid(node):
 				node.stop()
 				node.free()
+
+
+
+static func register_assert(value):
+	var asserts :Array = GdUnitSingleton.instance("GdUnitAsserts", func(): return [])
+	asserts.push_back(value)
+
+
+static func release_asserts():
+	var asserts :Array = GdUnitSingleton.instance("GdUnitAsserts", func(): return [])
+	for index in range(asserts.size()-1, -1, -1):
+		var assert_ = asserts[index]
+		assert_.notification(Object.NOTIFICATION_PREDELETE)
+		asserts.remove_at(index)
+
+
+# the finally cleaup unfreed resources and singletons
+static func dispose_all():
+	release_timers()
+	release_asserts()
+	GdUnitSignals.dispose()
+	GdUnitSingleton.dispose()
 
 
 # if instance an mock or spy we need manually freeing the self reference

addons/gdUnit4/bin/GdUnitCmdTool.gd
@@ -95,8 +95,12 @@ class CLIRunner extends Node:
 			_executor.free()
 		if is_instance_valid(_rtf):
 			_rtf.free()
-		GdUnitSignals.dispose()
-		GdUnitSingleton.dispose()
+		GdUnitTools.dispose_all()
+		await get_tree().physics_frame
+		prints("-Orphan nodes report-----------------------")
+		Window.print_orphan_nodes()
+		prints("-SceneTree report-----------------------")
+		get_tree().root.print_tree_pretty()
 		get_tree().quit(code)
 	
 	

addons/gdUnit4/src/GdUnitAwaiter.gd
@@ -49,7 +49,9 @@ static func await_signal_idle_frames(source :Object, signal_name :String, args :
 # use this waiter and not `await get_tree().create_timer().timeout to prevent errors when a test case is timed out
 static func await_millis(milliSec :int) -> void:
 	var timer :Timer = Timer.new()
+	timer.set_name("gdunit_await_millis_timer_%d" % timer.get_instance_id())
 	Engine.get_main_loop().root.add_child(timer)
+	timer.add_to_group("GdUnitTimers")
 	timer.set_one_shot(true)
 	timer.start(milliSec * 0.001)
 	await timer.timeout

addons/gdUnit4/src/asserts/GdUnitSignalAssertImpl.gd
@@ -59,7 +59,7 @@ class SignalCollector extends GdUnitSingleton:
 	# unregister all acquired resources/connections, otherwise it ends up in orphans
 	# is called when the emitter is removed from the parent
 	func unregister_emitter(emitter :Object):
-		GdUnitTools.release_connections(emitter)
+		GdUnitTools._release_connections(emitter)
 		if is_instance_valid(emitter):
 			_collected_signals.erase(emitter)
 	

addons/gdUnit4/src/core/GdObjects.gd
@@ -255,8 +255,6 @@ static func _equals(obj_a, obj_b, case_sensitive :bool, deep_check :bool, deep_s
 @warning_ignore("shadowed_variable_base_class")
 static func notification_as_string(instance :Variant, notification :int) -> String:
 	var error := "Unknown notification: '%s' at instance:  %s" % [notification, instance]
-	if instance == null:
-		return error
 	if instance is Node:
 		return NOTIFICATION_AS_STRING_MAPPINGS[TYPE_NODE].get(notification, error)
 	if instance is Control:

addons/gdUnit4/src/core/GdUnitExecutor.gd
@@ -2,6 +2,7 @@ extends Node
 
 signal ExecutionCompleted()
 
+
 const INIT = 0
 const STAGE_TEST_SUITE_BEFORE = GdUnitReportCollector.STAGE_TEST_SUITE_BEFORE
 const STAGE_TEST_SUITE_AFTER = GdUnitReportCollector.STAGE_TEST_SUITE_AFTER
@@ -16,9 +17,6 @@ var _memory_pool :GdUnitMemoryPool = GdUnitMemoryPool.new()
 var _report_errors_enabled :bool
 var _report_collector : = GdUnitReportCollector.new()
 
-# we hold here the main instance for the argument matcher (singleton)
-@warning_ignore("unused_private_class_variable")
-var _arument_matcher = GdUnitArgumentMatchers.new()
 
 var _total_test_execution_orphans :int
 var _total_test_warnings :int
@@ -357,6 +355,7 @@ func clone_test_suite(test_suite :GdUnitTestSuite) -> GdUnitTestSuite:
 
 
 func dispose_timers(test_suite :GdUnitTestSuite):
+	GdUnitTools.release_timers()
 	for child in test_suite.get_children():
 		if child is Timer:
 			child.stop()

addons/gdUnit4/src/core/GdUnitMemoryPool.gd
@@ -100,7 +100,10 @@ func orphan_nodes() -> int:
 
 # register an instance to be freed when a test suite is finished
 static func register_auto_free(obj, pool :POOL) -> Variant:
-	if not is_instance_valid(obj):
+	# do not register on GDScriptNativeClass
+	if typeof(obj) == TYPE_OBJECT and (obj as Object).is_class("GDScriptNativeClass") :
+		return obj
+	if obj is GDScript or obj is ScriptExtension:
 		return obj
 	if obj is MainLoop:
 		push_error("avoid to add mainloop to auto_free queue  %s" % obj)

commit 1a9911d6729595a649a2425f28bf98f8d2129e8d

GD-224: Update project to Godot 4.1.1 (#223)
# Why
We want to support the latest Godot version v4.1.1


# What
- update project to v4.1.1
- add Godot 4.1, 4.1.1 to ci-pr and ci-dev workflows
- update README.md
- fix crash by remove the temporary workaround introduced by
https://github.com/MikeSchulze/gdUnit4/pull/137
- fix crash by do not print messages on plugin `_exit_tree`
(https://github.com/godotengine/godot/issues/79379)
```
malloc(): mismatching next->prev_size (unsorted)
Aborted (core dumped)
Error: Process completed with exit code 134.
```

.github/actions/upload-test-report/action.yml
@@ -13,4 +13,7 @@ runs:
       uses: actions/upload-artifact@v3
       with:
         name: test_report_${{ inputs.report-name }}
-        path: reports/**
+        path: |
+          reports/**
+          /var/lib/systemd/coredump/**
+          /var/log/syslog

.github/workflows/ci-dev.yml
@@ -21,7 +21,7 @@ jobs:
       fail-fast: false
       max-parallel: 10
       matrix:
-        godot-version: ['4.0.1', '4.0.2', '4.0.3']
+        godot-version: ['4.0.1', '4.0.2', '4.0.3', '4.1', '4.1.1']
 
     name: "CI on Godot 🐧 v${{ matrix.godot-version }}"
     uses: ./.github/workflows/unit-tests.yml

.github/workflows/ci-pr.yml
@@ -22,7 +22,7 @@ jobs:
       fail-fast: false
       max-parallel: 10
       matrix:
-        godot-version: ['4.0.1', '4.0.2', '4.0.3']
+        godot-version: ['4.0.1', '4.0.2', '4.0.3', '4.1', '4.1.1']
 
     name: "CI on Godot 🐧 v${{ matrix.godot-version }}"
     uses: ./.github/workflows/unit-tests.yml

addons/gdUnit4/bin/ProjectScanner.gd
@@ -11,8 +11,11 @@ func _initialize():
 
 
 func _finalize():
-	prints("Finalize scanner ..")
+	if Engine.get_version_info().hex < 0x40100 or Engine.get_version_info().hex > 0x40101:
+		print("Finalize scanner ..")
 	scanner.free()
+	if Engine.get_version_info().hex < 0x40100 or Engine.get_version_info().hex > 0x40101:
+		prints("done")
 
 
 class ProjectScanner extends Node:
@@ -48,7 +51,7 @@ class ProjectScanner extends Node:
 			_console.prints_color("======================================", Color.CORNFLOWER_BLUE)
 			_console.new_line()
 			await get_tree().process_frame
-			get_tree().quit()
+			get_tree().quit(0)
 	
 	
 	func scan_project() -> void:

addons/gdUnit4/plugin.gd
@@ -50,4 +50,5 @@ func _exit_tree():
 	GdUnitTools.dispose_all()
 	if Engine.has_meta("GdUnitEditorPlugin"):
 		Engine.remove_meta("GdUnitEditorPlugin")
-	prints("Unload GdUnit4 Plugin success")
+	if Engine.get_version_info().hex < 0x40100 or Engine.get_version_info().hex > 0x40101:
+		prints("Unload GdUnit4 Plugin success")

addons/gdUnit4/src/asserts/GdUnitFuncAssertImpl.gd
@@ -16,8 +16,6 @@ var _sleep_timer :Timer = null
 
 
 func _init(instance :Object, func_name :String, args := Array()):
-	# temporary workaround to hold the instance see https://github.com/godotengine/godot/issues/73889
-	GdUnitTools.register_assert(self)
 	_line_number = GdUnitAssertImpl._get_line_number()
 	GdAssertReports.reset_last_error_line_number()
 	# save the actual assert instance on the current thread context
@@ -29,16 +27,9 @@ func _init(instance :Object, func_name :String, args := Array()):
 		_current_value_provider = CallBackValueProvider.new(instance, func_name, args)
 
 
-func _to_string():
-	return "GdUnitFuncAssertImpl" + str(get_instance_id())
-
-
 func _notification(_what):
 	if is_instance_valid(self):
 		dispose()
-	# temporary workaround to hold the instance see https://github.com/godotengine/godot/issues/73889
-	while is_instance_valid(self) and get_reference_count() > 1:
-		unreference()
 
 
 func report_success() -> GdUnitAssert:

addons/gdUnit4/src/core/GdUnitTools.gd
@@ -248,24 +248,9 @@ static func release_timers():
 				node.free()
 
 
-
-static func register_assert(value):
-	var asserts :Array = GdUnitSingleton.instance("GdUnitAsserts", func(): return [])
-	asserts.push_back(value)
-
-
-static func release_asserts():
-	var asserts :Array = GdUnitSingleton.instance("GdUnitAsserts", func(): return [])
-	for index in range(asserts.size()-1, -1, -1):
-		var assert_ = asserts[index]
-		assert_.notification(Object.NOTIFICATION_PREDELETE)
-		asserts.remove_at(index)
-
-
 # the finally cleaup unfreed resources and singletons
 static func dispose_all():
 	release_timers()
-	release_asserts()
 	GdUnitSignals.dispose()
 	GdUnitSingleton.dispose()
 

project.godot
@@ -11,7 +11,7 @@ config_version=5
 [application]
 
 config/name="gdUnit4"
-config/features=PackedStringArray("4.0")
+config/features=PackedStringArray("4.1")
 config/icon="res://icon.png"
 
 [audio]
@@ -20,6 +20,7 @@ default_bus_layout=""
 
 [debug]
 
+file_logging/enable_file_logging=true
 gdscript/warnings/unused_signal=false
 gdscript/warnings/return_value_discarded=false
 