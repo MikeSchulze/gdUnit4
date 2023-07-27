abb8299892b3537238d37892f4e4d3bb8bce6687
Nullpointer <mikeschulze.lpz@kabelmail.de>
2023-07-27 17:44:55 +0200

    GD-233: Fix font loading error on linux systems (#236)
    # Why
    We see an error `res://addons/gdUnit4/src/ui/GdUnitFonts.gd:38 - Invalid
    set index 'font_data' (on base: 'FontFile') with value of type
    'FontFile'.` on linux systems when activating the plugin
    
    # What
    The property `font_data` was removed and the error was just ignored.
    - Rewrite the font loader by load the font and duplicate and set the new
    font size.
    - Renamed to better related name
    - Add test coverage

---
+++ addons/gdUnit4/test/ui/GdUnitFontsTest.gd
@@ -0,0 +1,17 @@
+# GdUnit generated TestSuite
+class_name GdUnitFontsTest
+extends GdUnitTestSuite
+@warning_ignore('unused_parameter')
+@warning_ignore('return_value_discarded')
+
+# TestSuite generated from
+const __source = 'res://addons/gdUnit4/src/ui/GdUnitFonts.gd'
+
+
+func test_load_and_resize_font() -> void:
+	var font8 = GdUnitFonts.load_and_resize_font(GdUnitFonts.FONT_MONO, 8)
+	var font16 = GdUnitFonts.load_and_resize_font(GdUnitFonts.FONT_MONO, 16)
+	
+	assert_object(font8).is_not_null().is_not_same(font16)
+	assert_that(font8.fixed_size).is_equal(8)
+	assert_that(font16.fixed_size).is_equal(16)
