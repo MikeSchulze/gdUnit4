061d058e089c78dc55fa8084156b973b4d32c4cb
Mike Schulze <mikeschulze.lpz@kabelmail.de>
2023-10-18 20:52:15 +0200

    fix error reporting for paramaterized test

--- addons/gdUnit4/src/core/execution/stages/parameterized/GdUnitTestCaseParameterizedTestStage.gd
+++ addons/gdUnit4/src/core/execution/stages/parameterized/GdUnitTestCaseParameterizedTestStage.gd
@@ -15,6 +15,9 @@ func _execute(context :GdUnitExecutionContext) -> void:
 	var test_case_parameters := test_case.test_parameters()
 	var test_parameter_index := test_case.test_parameter_index()
 	var test_case_names := test_case.test_case_names()
+	var is_fail := false
+	var is_error := false
+	var failing_index := 0
 	
 	for test_case_index in test_case.test_parameters().size():
 		# is test_parameter_index is set, we run this parameterized test only
@@ -29,9 +32,17 @@ func _execute(context :GdUnitExecutionContext) -> void:
 		await test_case.execute_paramaterized(test_case_parameters[test_case_index])
 		await _stage_after.execute(test_context)
 		# we need to clean up the reports here so they are not reported twice
+		is_fail = is_fail or test_context.count_failures(false) > 0
+		is_error = is_error or test_context.count_errors(false) > 0
+		failing_index = test_case_index - 1
 		test_context.reports().clear()
 		if test_case.is_interupted():
 			break
+	# add report to parent execution context if failed or an error is found
+	if is_fail:
+		context.reports().append(GdUnitReport.new().create(GdUnitReport.FAILURE, test_case.line_number(), "Test failed at parameterized index %d." % failing_index))
+	if is_error:
+		context.reports().append(GdUnitReport.new().create(GdUnitReport.ABORT, test_case.line_number(), "Test aborted at parameterized index %d." % failing_index))
 	await context.gc()
 
 
