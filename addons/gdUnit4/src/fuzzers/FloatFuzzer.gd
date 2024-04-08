aedec38546511a6a0aa84f089653d1aa542a9610
Keir <keir@sneezey.uk>
2024-04-08 14:27:01 +0100

    fix: use float instead of int

--- addons/gdUnit4/src/fuzzers/FloatFuzzer.gd
+++ addons/gdUnit4/src/fuzzers/FloatFuzzer.gd
@@ -1,14 +1,13 @@
 class_name FloatFuzzer
 extends Fuzzer
 
-var _from: int = 0
-var _to: int = 0
+var _from: float = 0
+var _to: float = 0
 
-func _init(from: int, to: int):
+func _init(from: float, to: float):
 	assert(from <= to, "Invalid range!")
 	_from = from
 	_to = to
 
 func next_value() -> Variant:
-	var value := randf_range(_from, _to)
-	return value
+	return randf_range(_from, _to)
