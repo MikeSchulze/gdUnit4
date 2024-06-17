1ee036bd861b9e67a1a7375e83d3f792ab2b008e
Nullpointer <mikeschulze.lpz@gmail.com>
2024-06-17 13:02:25 +0200

    GD-508: Revisit the EditorFileSystemContextMenuHandler added to the root node
    # Why
    see https://github.com/MikeSchulze/gdUnit4/issues/508
    
    # What
    - simplify `EditorFileSystemContextMenuHandler`
    - do preload and add as child to the GdUnit inspector

--- addons/gdUnit4/src/ui/GdUnitInspector.gd
+++ addons/gdUnit4/src/ui/GdUnitInspector.gd
@@ -3,6 +3,7 @@ class_name GdUnitInspecor
 extends Panel
 
 const ScriptEditorContextMenuHandler = preload("res://addons/gdUnit4/src/ui/menu/ScriptEditorContextMenuHandler.gd")
+const EditorFileSystemContextMenuHandler = preload("res://addons/gdUnit4/src/ui/menu/EditorFileSystemContextMenuHandler.gd")
 
 var _command_handler := GdUnitCommandHandler.instance()
 
@@ -19,11 +20,6 @@ func _ready() -> void:
 	if Engine.is_editor_hint():
 		add_script_editor_context_menu()
 		add_file_system_dock_context_menu()
-
-
-func _notification(what: int) -> void:
-	if what == NOTIFICATION_PREDELETE:
-		EditorFileSystemControls.unregister_context_menu()
 
 
 func _process(_delta: float) -> void:
@@ -52,13 +48,13 @@ func _getEditorThemes() -> void:
 func add_file_system_dock_context_menu() -> void:
 	var is_test_suite := func is_visible(script: Script, is_ts: bool) -> bool:
 		if script == null:
-			return true
+			return false
 		return GdObjects.is_test_suite(script) == is_ts
 	var menu :Array[GdUnitContextMenuItem] = [
 		GdUnitContextMenuItem.new(GdUnitContextMenuItem.MENU_ID.TEST_RUN, "Run Testsuites", "Play", is_test_suite.bind(true), _command_handler.command(GdUnitCommandHandler.CMD_RUN_TESTSUITE)),
 		GdUnitContextMenuItem.new(GdUnitContextMenuItem.MENU_ID.TEST_DEBUG, "Debug Testsuites", "PlayStart", is_test_suite.bind(true), _command_handler.command(GdUnitCommandHandler.CMD_RUN_TESTSUITE_DEBUG)),
 	]
-	EditorFileSystemControls.register_context_menu(menu)
+	add_child(EditorFileSystemContextMenuHandler.new(menu))
 
 
 func add_script_editor_context_menu() -> void:
