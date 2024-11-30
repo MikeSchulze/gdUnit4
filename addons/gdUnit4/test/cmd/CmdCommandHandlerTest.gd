7ad82a56663c4f46ed2790a5f779d4a66ad91496
Nullpointer <mikeschulze.lpz@gmail.com>
2024-11-30 14:07:39 +0100

    GD-605: Fixes the CMD tool to collect multipe command arguments (#606)
    # Why
    The CMD tool had an issue to collect all given arguments. e.g. using
    --add <value> or --i <value> multiple times it only applyes the first
    argument example:
    `./addons/gdUnit4/runtest.sh --add res://addons/gdUnit4/test/core/parse/
    --add res://addons/gdUnit4/test/asserts/GdUnitAssertImplTest.gd -i
    GdDefaultValueDecoderTest -i GdScriptParserTest`
    
    # What
    - fixes the root cause on `CmdCommandHandler#execute` to call the
    correct callable to handle collected arguments
      - add register callback validations to the handler
    - code formattings
    - remove obsolete code `_enhanced_fr_test `

--- addons/gdUnit4/test/cmd/CmdCommandHandlerTest.gd
+++ addons/gdUnit4/test/cmd/CmdCommandHandlerTest.gd
@@ -11,17 +11,17 @@ var _cmd_instance: TestCommands
 
 # small example of command class
 class TestCommands:
-	func cmd_a() -> String:
-		return "cmd_a"
+	func cmd_no_arg() -> String:
+		return ""
 
 	func cmd_foo() -> String:
 		return "cmd_foo"
 
-	func cmd_bar(value :String) -> String:
+	func cmd_arg(value: String) -> String:
 		return value
 
-	func cmd_bar2(value_a: String, value_b: String) -> Array:
-		return [value_a, value_b]
+	func cmd_args(values: PackedStringArray) -> Array:
+		return values
 
 	func cmd_x() -> String:
 		return "cmd_x"
@@ -32,14 +32,53 @@ func before() -> void:
 	_cmd_options = CmdOptions.new([
 		CmdOption.new("-a", "some help text a", "some description a"),
 		CmdOption.new("-f, --foo", "some help text foo", "some description foo"),
-		CmdOption.new("-b, --bar", "some help text bar", "some description bar"),
-		CmdOption.new("-b2, --bar2", "some help text bar", "some description bar"),
+		CmdOption.new("-arg, --arg", "some help text bar", "some description bar")
 	],
 	# advnaced options
 	[
 		CmdOption.new("-x", "some help text x", "some description x"),
 	])
 	_cmd_instance = TestCommands.new()
+
+
+func test_register_cb() -> void:
+	var cmd_handler: = CmdCommandHandler.new(_cmd_options)
+
+	# register a single argumend cb
+	cmd_handler.register_cb("-a", _cmd_instance.cmd_arg)
+	assert_dict(cmd_handler._command_cbs).contains_key_value("-a", [_cmd_instance.cmd_arg, CmdCommandHandler.NO_CB])
+
+
+func test_register_invalid_cb() -> void:
+	var cmd_handler: = CmdCommandHandler.new(_cmd_options)
+	# try to register a multi argumend cb
+	cmd_handler.register_cb("-a", _cmd_instance.cmd_args)
+	# verify the cb is not registered
+	assert_dict(cmd_handler._command_cbs).is_empty()
+
+
+func test_register_cbv() -> void:
+	var cmd_handler: = CmdCommandHandler.new(_cmd_options)
+
+	# register a single argumend cb
+	cmd_handler.register_cbv("-a", _cmd_instance.cmd_args)
+	assert_dict(cmd_handler._command_cbs).contains_key_value("-a", [CmdCommandHandler.NO_CB, _cmd_instance.cmd_args])
+
+
+func test_register_invalid_cbv() -> void:
+	var cmd_handler: = CmdCommandHandler.new(_cmd_options)
+	# try to register a single argumend cb
+	cmd_handler.register_cbv("-a", _cmd_instance.cmd_arg)
+	# verify the cb is not registered
+	assert_dict(cmd_handler._command_cbs).is_empty()
+
+
+func test_register_cb_and_cbv() -> void:
+	var cmd_handler: = CmdCommandHandler.new(_cmd_options)
+
+	cmd_handler.register_cb("-a", _cmd_instance.cmd_arg)
+	cmd_handler.register_cbv("-a", _cmd_instance.cmd_args)
+	assert_dict(cmd_handler._command_cbs).contains_key_value("-a", [_cmd_instance.cmd_arg, _cmd_instance.cmd_args])
 
 
 func test__validate_no_registerd_commands() -> void:
@@ -50,19 +89,19 @@ func test__validate_no_registerd_commands() -> void:
 
 func test__validate_registerd_commands() -> void:
 	var cmd_handler: = CmdCommandHandler.new(_cmd_options)
-	cmd_handler.register_cb("-a", Callable(_cmd_instance, "cmd_a"))
-	cmd_handler.register_cb("-f", Callable(_cmd_instance, "cmd_foo"))
-	cmd_handler.register_cb("-b", Callable(_cmd_instance, "cmd_bar"))
+	cmd_handler.register_cb("-a", _cmd_instance.cmd_no_arg)
+	cmd_handler.register_cb("-f", _cmd_instance.cmd_foo)
+	cmd_handler.register_cb("-arg", _cmd_instance.cmd_arg)
 
 	assert_result(cmd_handler._validate()).is_success()
 
 
 func test__validate_registerd_unknown_commands() -> void:
 	var cmd_handler: = CmdCommandHandler.new(_cmd_options)
-	cmd_handler.register_cb("-a", Callable(_cmd_instance, "cmd_a"))
-	cmd_handler.register_cb("-d", Callable(_cmd_instance, "cmd_foo"))
-	cmd_handler.register_cb("-b", Callable(_cmd_instance, "cmd_bar"))
-	cmd_handler.register_cb("-y", Callable(_cmd_instance, "cmd_x"))
+	cmd_handler.register_cb("-a", _cmd_instance.cmd_no_arg)
+	cmd_handler.register_cb("-d", _cmd_instance.cmd_foo)
+	cmd_handler.register_cb("-arg", _cmd_instance.cmd_arg)
+	cmd_handler.register_cb("-y", _cmd_instance.cmd_x)
 
 	assert_result(cmd_handler._validate())\
 		.is_error()\
@@ -71,23 +110,21 @@ func test__validate_registerd_unknown_commands() -> void:
 
 func test__validate_registerd_invalid_callbacks() -> void:
 	var cmd_handler := CmdCommandHandler.new(_cmd_options)
-	cmd_handler.register_cb("-a", Callable(_cmd_instance, "cmd_a"))
-	cmd_handler.register_cb("-f")
-	cmd_handler.register_cb("-b", Callable(_cmd_instance, "cmd_not_exists"))
+	cmd_handler.register_cb("-a", _cmd_instance.cmd_no_arg)
+	cmd_handler.register_cb("-arg", Callable(_cmd_instance, "cmd_not_exists"))
 
 	assert_result(cmd_handler._validate())\
 		.is_error()\
-		.contains_message("Invalid function reference for command '-b', Check the function reference!")
+		.contains_message("Invalid function reference for command '-arg', Check the function reference!")
 
 
 func test__validate_registerd_register_same_callback_twice() -> void:
 	var cmd_handler: = CmdCommandHandler.new(_cmd_options)
-	cmd_handler.register_cb("-a", Callable(_cmd_instance, "cmd_a"))
-	cmd_handler.register_cb("-b", Callable(_cmd_instance, "cmd_a"))
-	if cmd_handler._enhanced_fr_test:
-		assert_result(cmd_handler._validate())\
-			.is_error()\
-			.contains_message("The function reference 'cmd_a' already registerd for command '-a'!")
+	cmd_handler.register_cb("-a", _cmd_instance.cmd_no_arg)
+	cmd_handler.register_cb("-arg", _cmd_instance.cmd_no_arg)
+	assert_result(cmd_handler._validate())\
+		.is_error()\
+		.contains_message("The function reference 'cmd_no_arg' already registerd for command '-a'!")
 
 
 func test_execute_no_commands() -> void:
@@ -100,24 +137,25 @@ func test_execute_commands_no_cb_registered() -> void:
 	assert_result(cmd_handler.execute([CmdCommand.new("-a")])).is_success()
 
 
+@warning_ignore("unsafe_method_access")
 func test_execute_commands_with_cb_registered() -> void:
 	var cmd_handler: = CmdCommandHandler.new(_cmd_options)
-	var cmd_spy: Object = spy(_cmd_instance)
+	var cmd_spy: TestCommands = spy(_cmd_instance)
 
-	cmd_handler.register_cb("-a", Callable(cmd_spy, "cmd_a"))
-	cmd_handler.register_cb("-b", Callable(cmd_spy, "cmd_bar"))
-	cmd_handler.register_cbv("-b2", Callable(cmd_spy, "cmd_bar2"))
+	cmd_handler.register_cb("-arg", cmd_spy.cmd_arg)
+	cmd_handler.register_cbv("-arg", cmd_spy.cmd_args)
+	cmd_handler.register_cb("-a", cmd_spy.cmd_no_arg)
 
 	assert_result(cmd_handler.execute([CmdCommand.new("-a")])).is_success()
-	verify(cmd_spy).cmd_a()
+	verify(cmd_spy).cmd_no_arg()
 	verify_no_more_interactions(cmd_spy)
 
 	reset(cmd_spy)
 	assert_result(cmd_handler.execute([
 		CmdCommand.new("-a"),
-		CmdCommand.new("-b", ["some_value"]),
-		CmdCommand.new("-b2", ["value1", "value2"])])).is_success()
-	verify(cmd_spy).cmd_a()
-	verify(cmd_spy).cmd_bar("some_value")
-	verify(cmd_spy).cmd_bar2("value1", "value2")
+		CmdCommand.new("-arg", ["some_value"]),
+		CmdCommand.new("-arg", ["value1", "value2"])])).is_success()
+	verify(cmd_spy).cmd_no_arg()
+	verify(cmd_spy).cmd_arg("some_value")
+	verify(cmd_spy).cmd_args(PackedStringArray(["value1", "value2"]))
 	verify_no_more_interactions(cmd_spy)
