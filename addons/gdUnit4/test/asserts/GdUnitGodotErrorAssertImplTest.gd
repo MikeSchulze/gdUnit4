87b7c30e947f3ac05cb47670a5f64f70cbca4916
Nullpointer <mikeschulze.lpz@gmail.com>
2025-07-25 21:22:09 +0200

    GD-699: Add support for argument matchers on assert_error (#807)
    # Why
    We want to test simply if a push_error is called without checking the
    content of the error message
    
    # What
    - Allow argument matcher as argument on `is_push_warning`,
    `is_push_error` and `is_runtime_error`
    - fix code formattings

--- addons/gdUnit4/test/asserts/GdUnitGodotErrorAssertImplTest.gd
+++ addons/gdUnit4/test/asserts/GdUnitGodotErrorAssertImplTest.gd
@@ -7,55 +7,6 @@ extends GdUnitTestSuite
 # TestSuite generated from
 const __source = 'res://addons/gdUnit4/src/asserts/GdUnitGodotErrorAssertImpl.gd'
 
-
-class GodotErrorTestClass:
-
-	func test(value :int) -> void:
-		match value:
-			0:
-				@warning_ignore("assert_always_true")
-				assert(true, "no error" )
-			1: # failing assert
-				await (Engine.get_main_loop() as SceneTree).process_frame
-				if OS.is_debug_build():
-					# do not break the debug session we simmulate a assert by writing the error manually
-					if Engine.get_version_info().hex >= 0x40400:
-						prints("""
-							SCRIPT ERROR: Assertion failed: this is an assert error
-							   at: GodotErrorTestClass.test (res://addons/gdUnit4/test/asserts/GdUnitGodotErrorAssertImplTest.gd:18)
-						""".dedent())
-					else:
-						prints("""
-							USER SCRIPT ERROR: Assertion failed: this is an assert error
-							   at: GodotErrorTestClass.test (res://addons/gdUnit4/test/asserts/GdUnitGodotErrorAssertImplTest.gd:18)
-						""".dedent())
-				else:
-					assert(false, "this is an assert error" )
-			2: # push_warning
-				push_warning('this is an push_warning')
-			3: # push_error
-				push_error('this is an push_error')
-				pass
-			4: # runtime error
-				if OS.is_debug_build():
-					# do not break the debug session we simmulate a assert by writing the error manually
-					if Engine.get_version_info().hex >= 0x40400:
-						prints("""
-							SCRIPT ERROR: Division by zero error in operator '/'.
-							   at: GodotErrorTestClass.test (res://addons/gdUnit4/test/asserts/GdUnitGodotErrorAssertImplTest.gd:32)
-						""".dedent())
-					else:
-						prints("""
-							USER SCRIPT ERROR: Division by zero error in operator '/'.
-							   at: GodotErrorTestClass.test (res://addons/gdUnit4/test/asserts/GdUnitGodotErrorAssertImplTest.gd:32)
-						""".dedent())
-				else:
-					var a := 0
-					@warning_ignore("integer_division")
-					@warning_ignore("unused_variable")
-					var x := 1/a
-
-
 var _save_is_report_push_errors :bool
 var _save_is_report_script_errors :bool
 
@@ -86,10 +37,11 @@ func test_invalid_callable() -> void:
 
 
 func test_is_success() -> void:
-	await assert_error(func() -> void: await GodotErrorTestClass.new().test(0)).is_success()
+	await assert_error(produce_assert_success).is_success()
 
 	var assert_ := await assert_failure_await(func() -> void:
-		await assert_error(func() -> void: await GodotErrorTestClass.new().test(1)).is_success())
+		await assert_error(produce_assert_error).is_success()
+	)
 	assert_.is_failed().has_message("""
 		Expecting: no error's are ocured.
 			but found: 'Assertion failed: this is an assert error'
@@ -97,11 +49,12 @@ func test_is_success() -> void:
 
 
 func test_is_assert_failed() -> void:
-	await assert_error(func() -> void: await GodotErrorTestClass.new().test(1))\
+	await assert_error(produce_assert_error)\
 		.is_runtime_error('Assertion failed: this is an assert error')
 
 	var assert_ := await assert_failure_await(func() -> void:
-		await assert_error(func() -> void: GodotErrorTestClass.new().test(0)).is_runtime_error('Assertion failed: this is an assert error'))
+		await assert_error(produce_assert_success).is_runtime_error('Assertion failed: this is an assert error')
+	)
 	assert_.is_failed().has_message("""
 		Expecting: a runtime error is triggered.
 			message: 'Assertion failed: this is an assert error'
@@ -110,24 +63,34 @@ func test_is_assert_failed() -> void:
 
 
 func test_is_push_warning() -> void:
-	await assert_error(func() -> void: GodotErrorTestClass.new().test(2))\
-		.is_push_warning('this is an push_warning')
+	await assert_error(produce_push_warning).is_push_warning('this is an push_warning')
 
 	var assert_ := await assert_failure_await(func() -> void:
-		await assert_error(func() -> void: GodotErrorTestClass.new().test(0)).is_push_warning('this is an push_warning'))
+		await assert_error(produce_push_warning).is_push_warning('this is an error')
+	)
 	assert_.is_failed().has_message("""
 		Expecting: push_warning() is called.
-			message: 'this is an push_warning'
-			found: no errors
+			message: 'this is an error'
+			found: this is an push_warning
 		""".dedent().trim_prefix("\n"))
 
 
-func test_is_push_error() -> void:
-	await assert_error(func() -> void: GodotErrorTestClass.new().test(3))\
-		.is_push_error('this is an push_error')
+func test_is_push_warning_using_argument_matcher() -> void:
+	await assert_error(produce_push_warning).is_push_warning(any())
+	await assert_error(produce_push_warning).is_push_warning(any_string())
 
 	var assert_ := await assert_failure_await(func() -> void:
-		await assert_error(func() -> void: GodotErrorTestClass.new().test(0)).is_push_error('this is an push_error'))
+		await assert_error(produce_push_warning).is_push_warning(any_int())
+	)
+	assert_.is_failed().has_message("Only 'any()' and 'any_string()' argument matchers are allowed!")
+
+
+func test_is_push_error() -> void:
+	await assert_error(produce_push_error).is_push_error('this is an push_error')
+
+	var assert_ := await assert_failure_await(func() -> void:
+		await assert_error(produce_assert_success).is_push_error('this is an push_error')
+	)
 	assert_.is_failed().has_message("""
 		Expecting: push_error() is called.
 			message: 'this is an push_error'
@@ -135,14 +98,83 @@ func test_is_push_error() -> void:
 		""".dedent().trim_prefix("\n"))
 
 
-func test_is_runtime_error() -> void:
-	await assert_error(func() -> void: GodotErrorTestClass.new().test(4))\
-		.is_runtime_error("Division by zero error in operator '/'.")
+func test_is_push_error_using_argument_matcher() -> void:
+	await assert_error(produce_push_error).is_push_error(any())
+	await assert_error(produce_push_error).is_push_error(any_string())
 
 	var assert_ := await assert_failure_await(func() -> void:
-		await assert_error(func() -> void: GodotErrorTestClass.new().test(0)).is_runtime_error("Division by zero error in operator '/'."))
+		await assert_error(produce_push_error).is_push_error(any_int())
+	)
+	assert_.is_failed().has_message("Only 'any()' and 'any_string()' argument matchers are allowed!")
+
+
+func test_is_runtime_error() -> void:
+	await assert_error(produce_runtime_error).is_runtime_error("Division by zero error in operator '/'.")
+
+	var assert_ := await assert_failure_await(func() -> void:
+		await assert_error(produce_assert_success).is_runtime_error("Division by zero error in operator '/'."))
 	assert_.is_failed().has_message("""
 		Expecting: a runtime error is triggered.
 			message: 'Division by zero error in operator '/'.'
 			found: no errors
 		""".dedent().trim_prefix("\n"))
+
+
+func test_is_runtime_error_using_argument_matcher() -> void:
+	await assert_error(produce_runtime_error).is_runtime_error(any())
+	await assert_error(produce_runtime_error).is_runtime_error(any_string())
+
+	var assert_ := await assert_failure_await(func() -> void:
+		await assert_error(produce_runtime_error).is_runtime_error(any_int())
+	)
+	assert_.is_failed().has_message("Only 'any()' and 'any_string()' argument matchers are allowed!")
+
+
+func produce_assert_success() -> void:
+	@warning_ignore("assert_always_true")
+	assert(true, "no error" )
+
+
+func produce_assert_error() -> void:
+	if OS.is_debug_build():
+		# do not break the debug session we simmulate a assert by writing the error manually
+		if Engine.get_version_info().hex >= 0x40400:
+			prints("""
+				SCRIPT ERROR: Assertion failed: this is an assert error
+				   at: GodotErrorTestClass.test (res://addons/gdUnit4/test/asserts/GdUnitGodotErrorAssertImplTest.gd:18)
+			""".dedent())
+		else:
+			prints("""
+				USER SCRIPT ERROR: Assertion failed: this is an assert error
+				   at: GodotErrorTestClass.test (res://addons/gdUnit4/test/asserts/GdUnitGodotErrorAssertImplTest.gd:18)
+			""".dedent())
+	else:
+		assert(false, "this is an assert error" )
+
+
+func produce_push_warning() -> void:
+	push_warning('this is an push_warning')
+
+
+func produce_push_error() -> void:
+	push_error('this is an push_error')
+
+
+func produce_runtime_error() -> void:
+	if OS.is_debug_build():
+		# do not break the debug session we simmulate a assert by writing the error manually
+		if Engine.get_version_info().hex >= 0x40400:
+			prints("""
+				SCRIPT ERROR: Division by zero error in operator '/'.
+				   at: GodotErrorTestClass.test (res://addons/gdUnit4/test/asserts/GdUnitGodotErrorAssertImplTest.gd:32)
+			""".dedent())
+		else:
+			prints("""
+				USER SCRIPT ERROR: Division by zero error in operator '/'.
+				   at: GodotErrorTestClass.test (res://addons/gdUnit4/test/asserts/GdUnitGodotErrorAssertImplTest.gd:32)
+			""".dedent())
+	else:
+		var a := 0
+		@warning_ignore("integer_division")
+		@warning_ignore("unused_variable")
+		var x := 1/a
