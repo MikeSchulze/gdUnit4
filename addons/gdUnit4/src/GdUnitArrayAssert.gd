3b6117606c87a3a7945e0ee73d28cada9cf85496
Nullpointer <mikeschulze.lpz@gmail.com>
2025-09-29 20:51:04 +0200

    GD-905: Convert base assert function to abstract (part7)
    # Why
    The GdUnit4 assertion API faked interfaces to represent a small class with class documentation without overloaded implementation code. With Godot 4.5, GDScript now supports abstract classes and functions, so we should switch to that.
    
    # What
    - Convert functions to abstract

--- addons/gdUnit4/src/GdUnitArrayAssert.gd
+++ addons/gdUnit4/src/GdUnitArrayAssert.gd
@@ -40,74 +40,56 @@ func is_not_equal_ignoring_case(expected :Variant) -> GdUnitArrayAssert:
 
 
 ## Verifies that the current Array is empty, it has a size of 0.
-func is_empty() -> GdUnitArrayAssert:
-	return self
+@abstract func is_empty() -> GdUnitArrayAssert
 
 
 ## Verifies that the current Array is not empty, it has a size of minimum 1.
-func is_not_empty() -> GdUnitArrayAssert:
-	return self
+@abstract func is_not_empty() -> GdUnitArrayAssert
+
 
 ## Verifies that the current Array is the same. [br]
 ## Compares the current by object reference equals
-@warning_ignore("unused_parameter", "shadowed_global_identifier")
-func is_same(expected :Variant) -> GdUnitArrayAssert:
-	return self
+@abstract func is_same(expected: Variant) -> GdUnitArrayAssert
 
 
 ## Verifies that the current Array is NOT the same. [br]
 ## Compares the current by object reference equals
-@warning_ignore("unused_parameter", "shadowed_global_identifier")
-func is_not_same(expected :Variant) -> GdUnitArrayAssert:
-	return self
+@abstract func is_not_same(expected: Variant) -> GdUnitArrayAssert
 
 
 ## Verifies that the current Array has a size of given value.
-@warning_ignore("unused_parameter")
-func has_size(expectd: int) -> GdUnitArrayAssert:
-	return self
+
+@abstract func has_size(expectd: int) -> GdUnitArrayAssert
 
 
 ## Verifies that the current Array contains the given values, in any order.[br]
 ## The values are compared by deep parameter comparision, for object reference compare you have to use [method contains_same]
-@warning_ignore("unused_parameter")
-func contains(expected :Variant) -> GdUnitArrayAssert:
-	return self
+@abstract func contains(expected: Variant) -> GdUnitArrayAssert
 
 
 ## Verifies that the current Array contains exactly only the given values and nothing else, in same order.[br]
 ## The values are compared by deep parameter comparision, for object reference compare you have to use [method contains_same_exactly]
-@warning_ignore("unused_parameter")
-func contains_exactly(expected :Variant) -> GdUnitArrayAssert:
-	return self
+@abstract func contains_exactly(expected: Variant) -> GdUnitArrayAssert
 
 
 ## Verifies that the current Array contains exactly only the given values and nothing else, in any order.[br]
 ## The values are compared by deep parameter comparision, for object reference compare you have to use [method contains_same_exactly_in_any_order]
-@warning_ignore("unused_parameter")
-func contains_exactly_in_any_order(expected :Variant) -> GdUnitArrayAssert:
-	return self
+@abstract func contains_exactly_in_any_order(expected: Variant) -> GdUnitArrayAssert
 
 
 ## Verifies that the current Array contains the given values, in any order.[br]
 ## The values are compared by object reference, for deep parameter comparision use [method contains]
-@warning_ignore("unused_parameter")
-func contains_same(expected :Variant) -> GdUnitArrayAssert:
-	return self
+@abstract func contains_same(expected: Variant) -> GdUnitArrayAssert
 
 
 ## Verifies that the current Array contains exactly only the given values and nothing else, in same order.[br]
 ## The values are compared by object reference, for deep parameter comparision use [method contains_exactly]
-@warning_ignore("unused_parameter")
-func contains_same_exactly(expected :Variant) -> GdUnitArrayAssert:
-	return self
+@abstract func contains_same_exactly(expected: Variant) -> GdUnitArrayAssert
 
 
 ## Verifies that the current Array contains exactly only the given values and nothing else, in any order.[br]
 ## The values are compared by object reference, for deep parameter comparision use [method contains_exactly_in_any_order]
-@warning_ignore("unused_parameter")
-func contains_same_exactly_in_any_order(expected :Variant) -> GdUnitArrayAssert:
-	return self
+@abstract func contains_same_exactly_in_any_order(expected: Variant) -> GdUnitArrayAssert
 
 
 ## Verifies that the current Array do NOT contains the given values, in any order.[br]
@@ -119,9 +101,7 @@ func contains_same_exactly_in_any_order(expected :Variant) -> GdUnitArrayAssert:
 ## # will fail
 ## assert_array([1, 2, 3, 4, 5]).not_contains([2, 6])
 ## [/codeblock]
-@warning_ignore("unused_parameter")
-func not_contains(expected :Variant) -> GdUnitArrayAssert:
-	return self
+@abstract func not_contains(expected: Variant) -> GdUnitArrayAssert
 
 
 ## Verifies that the current Array do NOT contains the given values, in any order.[br]
@@ -133,22 +113,17 @@ func not_contains(expected :Variant) -> GdUnitArrayAssert:
 ## # will fail
 ## assert_array([1, 2, 3, 4, 5]).not_contains([2, 6])
 ## [/codeblock]
-@warning_ignore("unused_parameter")
-func not_contains_same(expected :Variant) -> GdUnitArrayAssert:
-	return self
+@abstract func not_contains_same(expected: Variant) -> GdUnitArrayAssert
 
 
 ## Extracts all values by given function name and optional arguments into a new ArrayAssert.
 ## If the elements not accessible by `func_name` the value is converted to `"n.a"`, expecting null values
-@warning_ignore("unused_parameter")
-func extract(func_name: String, args := Array()) -> GdUnitArrayAssert:
-	return self
+@abstract func extract(func_name: String, args := Array()) -> GdUnitArrayAssert
 
 
 ## Extracts all values by given extractor's into a new ArrayAssert.
 ## If the elements not extractable than the value is converted to `"n.a"`, expecting null values
-@warning_ignore("unused_parameter")
-func extractv(
+@abstract func extractv(
 	extractor0 :GdUnitValueExtractor,
 	extractor1 :GdUnitValueExtractor = null,
 	extractor2 :GdUnitValueExtractor = null,
@@ -158,5 +133,4 @@ func extractv(
 	extractor6 :GdUnitValueExtractor = null,
 	extractor7 :GdUnitValueExtractor = null,
 	extractor8 :GdUnitValueExtractor = null,
-	extractor9 :GdUnitValueExtractor = null) -> GdUnitArrayAssert:
-	return self
+	extractor9 :GdUnitValueExtractor = null) -> GdUnitArrayAssert
