fe362dc8c62f60f2e1b164ab26c3330de385c709
DragonAxe <DragonAxe@users.noreply.github.com>
2025-09-29 11:32:42 -0600

    GD-912: fix(delete_directory): Remove hidden files (#901)
    # Why
    Hidden files will prevent a directory from being deleted which causes
    errors to be thrown in the console.
    For example, the following file was causing the following errors:
    `/home/<username>/.local/share/godot/app_userdata/<project_name>/tmp/update/addons/gdUnit4/src/reporters/html/template/.gdignore`
    ```
    ERROR: Delete user://tmp/update/addons/gdUnit4/src/reporters/html/template failed: Failed
       at: push_error (core/variant/variant_utility.cpp:1098)
    ERROR: Delete user://tmp/update/addons/gdUnit4/src/reporters/html failed: Failed
       at: push_error (core/variant/variant_utility.cpp:1098)
    ERROR: Delete user://tmp/update/addons/gdUnit4/src/reporters failed: Failed
       at: push_error (core/variant/variant_utility.cpp:1098)
    ERROR: Delete user://tmp/update/addons/gdUnit4/src failed: Failed
       at: push_error (core/variant/variant_utility.cpp:1098)
    ERROR: Delete user://tmp/update/addons/gdUnit4 failed: Failed
       at: push_error (core/variant/variant_utility.cpp:1098)
    ERROR: Delete user://tmp/update/addons failed: Failed
       at: push_error (core/variant/variant_utility.cpp:1098)
    ERROR: Delete user://tmp/update failed: Failed
       at: push_error (core/variant/variant_utility.cpp:1098)
    ERROR: Delete user://tmp failed: Failed
       at: push_error (core/variant/variant_utility.cpp:1098)
    ```
    I'm not sure how `.gdignore` was created in the first place.
    
    # What
    Set DirAccess include_hidden = true in `delete_directory` functions.
    
    ---------
    
    Co-authored-by: Nullpointer <mikeschulze.lpz@gmail.com>

--- addons/gdUnit4/test/core/GdUnitFileAccessTest.gd
+++ addons/gdUnit4/test/core/GdUnitFileAccessTest.gd
@@ -161,6 +161,7 @@ func test_delete_directory() -> void:
 	create_temp_dir("test_delete_dir/data2")
 	_create_file("test_delete_dir", "example_a.txt")
 	_create_file("test_delete_dir", "example_b.txt")
+	_create_file("test_delete_dir", ".hidden_file.txt")
 	_create_file("test_delete_dir/data1", "example.txt")
 	_create_file("test_delete_dir/data2", "example2.txt")
 
@@ -168,7 +169,8 @@ func test_delete_directory() -> void:
 		"data1",
 		"data2",
 		"example_a.txt",
-		"example_b.txt"
+		"example_b.txt",
+		".hidden_file.txt"
 	])
 
 	# Delete the entire directory and its contents
