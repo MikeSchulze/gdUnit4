221ec4415b84e64f52638619d0253209d8e18963
Nullpointer <mikeschulze.lpz@gmail.com>
2025-12-05 17:56:48 +0100

    GD-982 Adding boolean fuzzer
    # Why
    A fuzzer to create bool is missing.
    
    # What
    - Adding the boolean fuzzer
    - Make `Fuzzer` class abstract
    - Return typed fuzzers and values
    - Improve class documentaion

--- addons/gdUnit4/test/fuzzers/GdUnitFuzzerValueInjectionTest.gd
+++ addons/gdUnit4/test/fuzzers/GdUnitFuzzerValueInjectionTest.gd
@@ -20,7 +20,7 @@ func min_value() -> int:
 	return 1
 
 
-func fuzzer() -> Fuzzer:
+func get_fuzzer() -> Fuzzer:
 	return Fuzzers.rangei(min_value(), max_value())
 
 
@@ -55,35 +55,31 @@ func after() -> void:
 var _fuzzer_instance_before : Fuzzer = null
 
 
-@warning_ignore("shadowed_variable", "unused_parameter")
-func test_fuzzer_has_same_instance_peer_iteration(fuzzer:=TestFuzzer.new(), fuzzer_iterations := 10) -> void:
+func test_fuzzer_has_same_instance_peer_iteration(fuzzer := TestFuzzer.new(), _fuzzer_iterations := 10) -> void:
 	_current_iterations["test_fuzzer_has_same_instance_peer_iteration"] += 1
 	assert_object(fuzzer).is_not_null()
 	if _fuzzer_instance_before != null:
-		assert_that(fuzzer).is_same(_fuzzer_instance_before)
+		assert_object(fuzzer).is_same(_fuzzer_instance_before)
 	_fuzzer_instance_before = fuzzer
 
 
-@warning_ignore("shadowed_variable", "unused_parameter")
 func test_fuzzer_iterations_default(fuzzer := Fuzzers.rangei(-23, 22)) -> void:
 	_current_iterations["test_fuzzer_iterations_default"] += 1
 	assert_object(fuzzer).is_not_null()
+	assert_int(fuzzer.next_value()).is_between(-23, 22)
 
 
-@warning_ignore("shadowed_variable", "unused_parameter")
-func test_fuzzer_iterations_custom_value(fuzzer := Fuzzers.rangei(-23, 22), fuzzer_iterations := 234, fuzzer_seed := 100) -> void:
+func test_fuzzer_iterations_custom_value(_fuzzer := Fuzzers.rangei(-23, 22), _fuzzer_iterations := 234, _fuzzer_seed := 100) -> void:
 	_current_iterations["test_fuzzer_iterations_custom_value"] += 1
 
 
-@warning_ignore("shadowed_variable", "unused_parameter")
-func test_fuzzer_inject_value(fuzzer := Fuzzers.rangei(-23, 22), fuzzer_iterations := 100) -> void:
+func test_fuzzer_inject_value(fuzzer := Fuzzers.rangei(-23, 22), _fuzzer_iterations := 100) -> void:
 	_current_iterations["test_fuzzer_inject_value"] += 1
 	assert_object(fuzzer).is_not_null()
 	assert_int(fuzzer.next_value()).is_between(-23, 22)
 
 
-@warning_ignore("shadowed_variable", "unused_parameter")
-func test_fuzzer_with_timeout(fuzzer := Fuzzers.rangei(-23, 22), fuzzer_iterations := 20, timeout := 100) -> void:
+func test_fuzzer_with_timeout(fuzzer := Fuzzers.rangei(-23, 22), _fuzzer_iterations := 20, _timeout := 100) -> void:
 	discard_error_interupted_by_timeout()
 	assert_int(fuzzer.next_value()).is_between(-23, 22)
 
@@ -92,40 +88,38 @@ func test_fuzzer_with_timeout(fuzzer := Fuzzers.rangei(-23, 22), fuzzer_iteratio
 	# we not expect more than 10 iterations it should be interuptead by a timeout
 	assert_int(fuzzer.iteration_index()).is_less_equal(10)
 
-var expected_value := [22, 3, -14, -16, 21, 20, 4, -23, -19, -5]
+var expected_value: Array[int] = [22, 3, -14, -16, 21, 20, 4, -23, -19, -5]
 
 
-@warning_ignore("shadowed_variable", "unused_parameter")
-func test_fuzzer_inject_value_with_seed(fuzzer := Fuzzers.rangei(-23, 22), fuzzer_iterations := 10, fuzzer_seed := 187772) -> void:
+func test_fuzzer_inject_value_with_seed(fuzzer := Fuzzers.rangei(-23, 22), _fuzzer_iterations := 10, _fuzzer_seed := 187772) -> void:
 	assert_object(fuzzer).is_not_null()
 	var iteration_index := fuzzer.iteration_index()-1
-	var current :int = fuzzer.next_value()
-	var expected :int = expected_value[iteration_index]
+	var current := fuzzer.next_value()
+	var expected := expected_value[iteration_index]
 	assert_int(iteration_index).is_between(0, 9).is_less(10)
 	assert_int(current)\
 		.override_failure_message("Expect value %s checked test iteration %s\n but was %s" % [expected, iteration_index, current])\
 		.is_equal(expected)
 
-var expected_value_a := [22, -14, 21, 4, -19, -11, 5, 21, -6, -9]
-var expected_value_b := [35, 38, 34, 39, 35, 41, 37, 35, 34, 39]
+var expected_value_a: Array[int] = [22, -14, 21, 4, -19, -11, 5, 21, -6, -9]
+var expected_value_b: Array[int] = [35, 38, 34, 39, 35, 41, 37, 35, 34, 39]
 
 
-@warning_ignore("shadowed_variable", "unused_parameter")
-func test_multiple_fuzzers_inject_value_with_seed(fuzzer_a := Fuzzers.rangei(-23, 22), fuzzer_b := Fuzzers.rangei(33, 44), fuzzer_iterations := 10, fuzzer_seed := 187772) -> void:
+func test_multiple_fuzzers_inject_value_with_seed(fuzzer_a := Fuzzers.rangei(-23, 22), fuzzer_b := Fuzzers.rangei(33, 44), _fuzzer_iterations := 10, _fuzzer_seed := 187772) -> void:
 	_current_iterations["test_multiple_fuzzers_inject_value_with_seed"] += 1
 	assert_object(fuzzer_a).is_not_null()
 	assert_object(fuzzer_b).is_not_null()
 	var iteration_index_a := fuzzer_a.iteration_index()-1
-	var current_a :int = fuzzer_a.next_value()
-	var expected_a :int = expected_value_a[iteration_index_a]
+	var current_a := fuzzer_a.next_value()
+	var expected_a := expected_value_a[iteration_index_a]
 	assert_int(iteration_index_a).is_between(0, 9).is_less(10)
 	assert_int(current_a).is_between(-23, 22)
 	assert_int(current_a)\
 		.override_failure_message("Expect value %s checked test iteration %s\n but was %s" % [expected_a, iteration_index_a, current_a])\
 		.is_equal(expected_a)
 	var iteration_index_b := fuzzer_b.iteration_index()-1
-	var current_b :int = fuzzer_b.next_value()
-	var expected_b :int = expected_value_b[iteration_index_b]
+	var current_b := fuzzer_b.next_value()
+	var expected_b := expected_value_b[iteration_index_b]
 	assert_int(iteration_index_b).is_between(0, 9).is_less(10)
 	assert_int(current_b).is_between(33, 44)
 	assert_int(current_b)\
@@ -133,8 +127,7 @@ func test_multiple_fuzzers_inject_value_with_seed(fuzzer_a := Fuzzers.rangei(-23
 		.is_equal(expected_b)
 
 
-@warning_ignore("shadowed_variable", "unused_parameter")
-func test_fuzzer_error_after_eight_iterations(fuzzer:=TestFuzzer.new(), fuzzer_iterations := 10) -> void:
+func test_fuzzer_error_after_eight_iterations(fuzzer:=TestFuzzer.new(), _fuzzer_iterations := 10) -> void:
 	assert_object(fuzzer).is_not_null()
 	# should fail after 8 iterations
 	if fuzzer.iteration_index() == 8:
@@ -145,22 +138,25 @@ func test_fuzzer_error_after_eight_iterations(fuzzer:=TestFuzzer.new(), fuzzer_i
 		assert_int(fuzzer.next_value()).is_between(0, 9)
 
 
-@warning_ignore("shadowed_variable", "unused_parameter")
-func test_fuzzer_custom_func(fuzzer:=fuzzer()) -> void:
+func test_fuzzer_custom_func(fuzzer := get_fuzzer()) -> void:
 	assert_object(fuzzer).is_not_null()
 	assert_int(fuzzer.next_value()).is_between(1, 10)
 
 
-@warning_ignore("shadowed_variable", "unused_parameter")
 func test_multiline_fuzzer_args(
 	fuzzer_a := Fuzzers.rangev2(Vector2(-47, -47), Vector2(47, 47)),
 	fuzzer_b := Fuzzers.rangei(0, 9),
-	fuzzer_iterations := 23) -> void:
+	_fuzzer_iterations := 23) -> void:
 		assert_object(fuzzer_a).is_not_null()
 		assert_object(fuzzer_b).is_not_null()
 		_current_iterations["test_multiline_fuzzer_args"] += 1
 
 
-@warning_ignore("untyped_declaration", "unused_parameter")
-func test_fuzzing_with_untyped_parameters(float_fuzzer = Fuzzers.rangef(-100.0, 100.0), fuzzer_iterations = 10):
+@warning_ignore("untyped_declaration")
+func test_fuzzing_with_untyped_parameters(float_fuzzer = Fuzzers.rangef(-100.0, 100.0), _fuzzer_iterations = 10):
 	assert_float(float_fuzzer.next_value()).is_between(-100.0, 100.0)
+
+
+func test_boolean_fuzzer(bool_fuzzer := Fuzzers.boolean(), _fuzzer_iterations := 50) -> void:
+	var value := bool_fuzzer.next_value()
+	assert_bool(value in [true, false]).is_true()
--- addons/gdUnit4/test/fuzzers/StringFuzzerTest.gd
+++ addons/gdUnit4/test/fuzzers/StringFuzzerTest.gd
@@ -4,20 +4,20 @@ extends GdUnitTestSuite
 
 
 func test_extract_charset() -> void:
-	assert_str(StringFuzzer.extract_charset("abc").to_byte_array().get_string_from_utf32()).is_equal("abc")
-	assert_str(StringFuzzer.extract_charset("abcDXG").to_byte_array().get_string_from_utf32()).is_equal("abcDXG")
-	assert_str(StringFuzzer.extract_charset("a-c").to_byte_array().get_string_from_utf32()).is_equal("abc")
-	assert_str(StringFuzzer.extract_charset("a-z").to_byte_array().get_string_from_utf32()).is_equal("abcdefghijklmnopqrstuvwxyz")
-	assert_str(StringFuzzer.extract_charset("A-Z").to_byte_array().get_string_from_utf32()).is_equal("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	assert_str(StringFuzzer._extract_charset("abc").to_byte_array().get_string_from_utf32()).is_equal("abc")
+	assert_str(StringFuzzer._extract_charset("abcDXG").to_byte_array().get_string_from_utf32()).is_equal("abcDXG")
+	assert_str(StringFuzzer._extract_charset("a-c").to_byte_array().get_string_from_utf32()).is_equal("abc")
+	assert_str(StringFuzzer._extract_charset("a-z").to_byte_array().get_string_from_utf32()).is_equal("abcdefghijklmnopqrstuvwxyz")
+	assert_str(StringFuzzer._extract_charset("A-Z").to_byte_array().get_string_from_utf32()).is_equal("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 	# unicode
-	assert_str(StringFuzzer.extract_charset("abc日本語DXG").to_byte_array().get_string_from_utf32()).is_equal("abc日本語DXG")
+	assert_str(StringFuzzer._extract_charset("abc日本語DXG").to_byte_array().get_string_from_utf32()).is_equal("abc日本語DXG")
 
 	# range token at start
-	assert_str(StringFuzzer.extract_charset("-a-dA-D2-8+_").to_byte_array().get_string_from_utf32()).is_equal("-abcdABCD2345678+_")
+	assert_str(StringFuzzer._extract_charset("-a-dA-D2-8+_").to_byte_array().get_string_from_utf32()).is_equal("-abcdABCD2345678+_")
 	# range token at end
-	assert_str(StringFuzzer.extract_charset("a-dA-D2-8+_-").to_byte_array().get_string_from_utf32()).is_equal("abcdABCD2345678+_-")
+	assert_str(StringFuzzer._extract_charset("a-dA-D2-8+_-").to_byte_array().get_string_from_utf32()).is_equal("abcdABCD2345678+_-")
 	# range token in the middle
-	assert_str(StringFuzzer.extract_charset("a-d-A-D2-8+_").to_byte_array().get_string_from_utf32()).is_equal("abcd-ABCD2345678+_")
+	assert_str(StringFuzzer._extract_charset("a-d-A-D2-8+_").to_byte_array().get_string_from_utf32()).is_equal("abcd-ABCD2345678+_")
 
 
 func test_next_value() -> void:
@@ -26,7 +26,7 @@ func test_next_value() -> void:
 	var r := RegEx.new()
 	r.compile("[%s]+" % pattern)
 	for i in 100:
-		var value :String = fuzzer.next_value()
+		var value := fuzzer.next_value()
 		# verify the generated value has a length in the configured min/max range
 		assert_int(value.length()).is_between(4, 128)
 		# using regex to remove_at all expected chars to verify the value only containing expected chars by is empty
@@ -37,7 +37,7 @@ func test_next_value_min_max_boundaries() -> void:
 	var boundaries := {}
 	var fuzzer := StringFuzzer.new(2, 3, "A")
 	for i in 200:
-		var value :String = fuzzer.next_value()
+		var value := fuzzer.next_value()
 		boundaries[value.length()] = boundaries.get_or_add(value.length(), 0)
 
 	# verify it contains only values with length 2 or 3
@@ -59,3 +59,8 @@ func test_next_value_min_max_same_boundaries() -> void:
 		.is_not_empty()\
 		.has_size(1)\
 		.contains_keys(2)
+
+
+func test_password(fuzzer := StringFuzzer.new(8, 32, "a-zA-Z0-9!@#$%"), _fuzzer_iterations := 100) -> void:
+	var password := fuzzer.next_value()
+	assert_str(password).has_length(8, Comparator.GREATER_EQUAL).has_length(32, Comparator.LESS_EQUAL)
